35ec8b7473e7184aec429696f78b3be2461e289b
SerjDrob <serjdrob@gmail.com>
2022-06-23 13:29:02 +0700

    BaseLaserProcess added

--- NewLaserProject/Classes/Process/LaserProcess.cs
+++ NewLaserProject/Classes/Process/LaserProcess.cs
@@ -145,4 +145,30 @@ namespace NewLaserProject.Classes
         }
     }
         
+    //public abstract class BaseLaserProcess
+    //{
+    //    private readonly ProgTreeParser _progTreeParser;
+    //    protected Func<Task> pierceFunction;
+
+    //    public BaseLaserProcess(ProgTreeParser progTreeParser)
+    //    {
+    //        _progTreeParser = progTreeParser;
+
+    //        _progTreeParser
+    //            .SetModuleFunction<TapperBlock>(FuncForTapperBlock)
+    //            .SetModuleFunction<AddZBlock>(FuncForAddZBlock)
+    //            .SetModuleFunction<PierceBlock>(FuncForPierseBlock)
+    //            .SetModuleFunction<DelayBlock>(FuncForDelayBlock);
+
+    //        pierceFunction = _progTreeParser
+    //           .GetTree()
+    //           .GetFunc();
+    //    }
+
+    //    protected abstract Task FuncForTapperBlock(double tapper);
+    //    protected abstract Task FuncForAddZBlock(double z);
+    //    protected abstract Task FuncForPierseBlock(ExtendedParams extendedParams);
+    //    protected abstract Task FuncForDelayBlock(int delay);
+
+    //}
 }
