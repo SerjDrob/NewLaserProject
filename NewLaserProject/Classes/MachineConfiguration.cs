02937e7b3608b704c95bdced4e9be24bd30af04d
SerjDrob <serjdrob@gmail.com>
2022-10-13 18:18:08 +0700

    minor changes

--- NewLaserProject/Classes/MachineConfiguration.cs
+++ NewLaserProject/Classes/MachineConfiguration.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft;
-
-namespace NewLaserProject.Classes
+﻿namespace NewLaserProject.Classes
 {
     internal class MachineConfiguration
     {
